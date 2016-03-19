e6d989ceb0600f7f2e074f5e256ab80c505f8a00
Anna Garcia <annaegarcia@gmail.com>
2016-03-18 20:57:17 -0400

    init

--- DemoApp/ViewController.swift
+++ DemoApp/ViewController.swift
@@ -10,16 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var changeColorButton: UIButton!
+     @IBOutlet weak var changeColorButton: UILabel!
+    let colorOptions ={UIColor.orangeColor(),UIColor.greenColor(),UIColor.redColor(),UIColor.blueColor(),
+    UIColor.blackColor()
+        
+    let currentColor = 0
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+   
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction didTapChangeColorButon(sender:AnyObject){
+        
+        currentColor +=1
+        if currentColor >+ color.Options.count {
+            currentColor = 0
+        }
+        
+        titleLabel.tectColor =colorOptions[currentColor]
+    
+    }
+        
+        
+        
+        
 }
 
